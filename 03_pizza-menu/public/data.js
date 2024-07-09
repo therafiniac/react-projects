65ac8f5964848618dbf8a10d3b7748e1166385e3
therafiniac <therafiniac@gmail.com>
2024-07-09 22:36:22 +0530

    03_pizza-menu project start

--- 03_pizza-menu/public/data.js
+++ 03_pizza-menu/public/data.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,52 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Menu/>
-      <Footer />
-    </div>
-  );
-}
-
-function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
-}
-
-function Menu() {
-  return(
-  <div>
-    <h2>Our menu</h2>
-    <Pizza />
-    <Pizza />
-    <Pizza />
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>{new Date().toLocaleTimeString()} We are currently open</footer>
-  );
-  // return React.createElement("footer", null, "We are currently open!");
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, mushrooms, and onion</p>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
