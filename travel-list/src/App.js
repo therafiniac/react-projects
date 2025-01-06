b0662507f78ce7146e77f6835d2c7ff032a6ce6f
therafiniac <therafiniac@gmail.com>
2025-01-06 18:22:49 +0530

    modifying app.js by lifting up state in travel-list project.

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,10 +25,9 @@ function App() {
 function Logo() {
   return <h1>🌴Far Away 💼</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -30,7 +35,7 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
-
+    onAddItems(newItem);
     setDescription("");
     setQuantity(1);
   }
@@ -57,11 +62,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
